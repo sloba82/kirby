eca1670f17fa300f4b728c2029715e07014960dc
sloba82 <dizajnistampa@gmail.com>
2020-11-21 10:46:23 +0100

    setup

---
+++ site/config/config.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * The config file is optional. It accepts a return array with config options
+ * Note: Never include more than one return statement, all options go within this single return array
+ * In this example, we set debugging to true, so that errors are displayed onscreen. 
+ * This setting must be set to false in production.
+ * All config options: https://getkirby.com/docs/reference/system/options
+ */
+return [
+    'api' => [
+       'basicAuth' => true,
+       'allowInsecure' => true,
+        'routes' => [
+            [
+              'pattern' => 'my-endpoint',
+              'action'  => function () {
+                return [
+                  'hello' => 'world'
+                ];
+              }
+            ]
+          ]
+        
+    ],
+    'debug' => true,
+    
+];
